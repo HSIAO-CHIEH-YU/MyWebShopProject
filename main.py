8cf31b9ee77753e4493ca2644881afa5ccccf839
HSIAO-CHIEH-YU <w038149@gmail.com>
2025-02-19 15:44:52 +0800

    還原點

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-#註冊
-NewuserName=str(input("請輸入欲註冊使用者名稱:"))
-NewuserPassWord=str(input("請輸入欲註冊使用者密碼:"))
-print("帳號註冊成功!!!")
-#登入
-userName=str(input("請輸入使用者名稱:"))
-userPassWord=str(input("請輸入使用者密碼:"))
-if(userName==NewuserName and userPassWord==NewuserPassWord):
-    print("歡迎用戶:"+userName+"回來!!")
-elif(userName!=NewuserName and userPassWord==NewuserPassWord):
-    print("找不到用戶!")
-elif(userName==NewuserName and userPassWord!=NewuserPassWord):
-    print("用戶:"+userName+"密碼錯誤!")
+def registr():#註冊
+    NewuserName=input("請輸入欲註冊使用者名稱:")
+    NewuserPassWord=input("請輸入欲註冊使用者密碼:")
+    print("帳號註冊成功!!!")
+def login():#登入
+    userName=input("請輸入使用者名稱:")
+    userPassWord=input("請輸入使用者密碼:")
+    if(userName==registr() and userPassWord==registr()):
+        print(f"歡迎用戶:{userName}回來!!")
+    elif(userName!=registr() and userPassWord==registr):
+        print("找不到用戶!")
+    elif(userName==registr() and userPassWord!=registr()):
+        print(f"用戶:{userName}密碼錯誤!")
 #pos系統
