d44e23814ec65b4b4a288425992d056359a8d5e4
HSIAO-CHIEH-YU <w038149@gmail.com>
2025-02-19 15:45:37 +0800

    還原點

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ def registr():#註冊
     NewuserName=input("請輸入欲註冊使用者名稱:")
     NewuserPassWord=input("請輸入欲註冊使用者密碼:")
     print("帳號註冊成功!!!")
-def login():#登入
+    return NewuserName,NewuserPassWord
+def login(Nreg_user,Nreg_pass):#登入
     userName=input("請輸入使用者名稱:")
     userPassWord=input("請輸入使用者密碼:")
-    if(userName==registr() and userPassWord==registr()):
+    if(userName==Nreg_user and userPassWord==Nreg_pass):
         print(f"歡迎用戶:{userName}回來!!")
-    elif(userName!=registr() and userPassWord==registr):
+    elif(userName!=Nreg_user and userPassWord==Nreg_pass):
         print("找不到用戶!")
-    elif(userName==registr() and userPassWord!=registr()):
+    elif(userName==Nreg_user and userPassWord!=Nreg_pass):
         print(f"用戶:{userName}密碼錯誤!")
+
+Nreg_user,Nreg_pass=registr()
+login(Nreg_user,Nreg_pass)#登入比對註冊的帳密
 #pos系統
