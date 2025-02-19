5f514b7d65c95a553f3eed848e11bdb3fc6778fd
HSIAO-CHIEH-YU <w038149@gmail.com>
2025-02-19 16:10:28 +0800

    還原點

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+users={}#會員(帳號:密碼)
+products={}#商品(商品名:售價)
+cart={}#購物車
+
 def registr():#註冊
     NewuserName=input("請輸入欲註冊使用者名稱:")
+    if NewuserName in users:
+        print(f"使用者{NewuserName}已經存在!")
+        return
     NewuserPassWord=input("請輸入欲註冊使用者密碼:")
+    users[NewuserName]=NewuserPassWord
     print("帳號註冊成功!!!")
-    return NewuserName,NewuserPassWord
-def login(Nreg_user,Nreg_pass):#登入
+    
+def login():#登入
     userName=input("請輸入使用者名稱:")
     userPassWord=input("請輸入使用者密碼:")
-    if(userName==Nreg_user and userPassWord==Nreg_pass):
-        print(f"歡迎用戶:{userName}回來!!")
-    elif(userName!=Nreg_user and userPassWord==Nreg_pass):
-        print("找不到用戶!")
-    elif(userName==Nreg_user and userPassWord!=Nreg_pass):
-        print(f"用戶:{userName}密碼錯誤!")
+    if(userName in users and users[userName]==userPassWord):
+        print(f"歡迎{userName}回來!")
+        return True
+    else:
+        print("登入失敗 帳號或密碼錯誤!")
+        return False
+#測試用   
+#registr()
+#login()
 
-Nreg_user,Nreg_pass=registr()
-login(Nreg_user,Nreg_pass)#登入比對註冊的帳密
 #pos系統
+def addProduct():#新增商品
+    productName=input("請輸入商品名稱:")
+    if productName in products:
+        print("商品已經存在!")
+        return
+    price=float(input("請輸入商品售價:"))
+    products[productName]=price
+    print(f"已新增商品{productName},{price}元")
+    
+        
+    
