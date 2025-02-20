753ff3a6387c9f85afbf532638dd2ae731f34b94
HSIAO-CHIEH-YU <w038149@gmail.com>
2025-02-20 16:44:39 +0800

    還原點

---
+++ mysql.py
@@ -0,0 +1,16 @@
+import mysql.connector
+conn=mysql.connector.connect
+{
+    host="localhost",#伺服器位置
+    user="root",     #sql使用者名稱
+    password="12345678",#sql密碼
+    database="shop_db"#sql要使用的資料庫名稱
+}
+
+talk=conn.cursor()#用來執行SQL語句。是MySQL資料庫的一個操作接口通過它來執行查詢 插入 更新等操作。
+
+def add_user(username,password):
+    query = "INSERT INTO users (username, password) VALUES (%s, %s)"#INSERT INTO用來向資料表中插入資料的SQL命令
+    talk.execute(query,(username,password))
+    conn.commit()#類似github的快照
+    print("用戶新增成功!")
---
+++ sql.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE shop_db;
+
+USE shop_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
