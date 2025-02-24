321b8bfb8c7a5c58961bb04a8c98fde4d4f57c3c
HSIAO-CHIEH-YU <w038149@gmail.com>
2025-02-24 11:11:52 +0800

    新增user_API跟product_API

--- fastAPI/product_API.py
+++ fastAPI/product_API.py
@@ -9,7 +9,8 @@ class Product(BaseModel):
     price:float
 
 @product_router.post("/add_product")
-async def add_product(product.name,product.price):
+async def add_product(product:Product):
+    mysqlPython.add_product(product.name,product.price)
     return{"message":f"商品:{product.name}以新增,售價為{product.price}元"}
 
 @product_router.get("/show_products")
