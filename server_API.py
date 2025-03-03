b230611ef21960eede4feac5cb55759972a9d867
HSIAO-CHIEH-YU <w038149@gmail.com>
2025-03-03 15:51:42 +0800

    註冊網頁開通api成功註冊帳號

--- server_API.py
+++ server_API.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastAPI.user_API import user_router# 從 fastAPI 資料夾中引入用戶相關的 API 路由
 from fastAPI.product_API import product_router# 從 fastAPI 資料夾中引入商品相關的 API 路由
 #user_router--->user_API裡面的user_router=APIRouter()
 #product_router--->product_API裡面的product_router=APIRouter()
 # 創建 FastAPI 應用實例
 app = FastAPI()
+
+# 設定 CORS 策略，允許特定來源的請求
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 設置為 "*" 代表允許來自所有域的請求
+    allow_credentials=True,
+    allow_methods=["*"],  # 允許所有的 HTTP 方法（GET, POST, PUT, DELETE 等）
+    allow_headers=["*"],  # 允許所有的請求標頭
+)
 
 # 註冊用戶相關的路由到 FastAPI 應用中
 # 這樣將 user_router 中的所有路由都加入到應用中
