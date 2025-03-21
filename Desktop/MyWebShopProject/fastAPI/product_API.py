d3b9543d821d9045f5adf14243ce6e211c802444
HSIAO-CHIEH-YU <w038149@gmail.com>
2025-03-21 14:22:23 +0800

    管理員新增刪除商品成功

---
+++ Desktop/MyWebShopProject/fastAPI/product_API.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+import mysqlPython  # type: ignore
+from typing import Optional
+
+product_router=APIRouter()
+
+class Product(BaseModel):
+    name:str
+    price:Optional[float]=None
+    have:Optional[int]=None
+    new_name:Optional[str]=None
+
+@product_router.post("/add_product")
+async def add_product(product:Product):
+    result=mysqlPython.add_product(product.name,product.price,product.have)
+    if result=="商品已存在":
+        return{"message":f"商品:{product.name}已存在"}
+    else:
+        return{"message":f"商品:{product.name}以新增,售價為{product.price}元,庫存數量有{product.have}個"}
+
+@product_router.get("/show_products")
+async def show_product():
+    products=mysqlPython.show_products()
+    return{"products":products}
+
+@product_router.delete("/delete_product/{product_name}")
+async def delete_product(product_name:str):
+    result=mysqlPython.delete_product_by_name(product_name)
+    if result=="商品不存在":
+        return{"message":f"商品:{product_name}不存在"}
+    else:
+        return{"message":f"商品:{product_name}已刪除"}
+    
+@product_router.put("/update_product/{name}")
+async def update_product(name: str, product: Product):
+    result = mysqlPython.update_product_details_by_name(name,new_name=product.new_name,new_price=product.price,new_have=product.have)
+    return {"message": result}
+
+@product_router.get("/show_cart")
+async def show_cart(user_id:int):
+    result=mysqlPython.show_cart(user_id)
+    return result
+
+@product_router.delete("/delete_cart")
+async def delete_cart(user_id:int,product_id:int):
+    result=mysqlPython.delete_from_cart(user_id,product_id)
+    return result
+
+@product_router.post("/buy_cart")
+async def buy_cart(user_id:int):
+    result=mysqlPython.buy(user_id)
+    return {"message":result}
---
+++ Desktop/MyWebShopProject/sql.sql
@@ -0,0 +1,40 @@
+create database shop_db;
+
+USE shop_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+    have INT NOT NULL DEFAULT 0  -- 新增庫存欄位
+);
+
+CREATE TABLE productsCar (
+    id INT AUTO_INCREMENT PRIMARY KEY,              -- 設置 `id` 為主鍵，並使用 AUTO_INCREMENT 讓它自動遞增
+    user_id INT NOT NULL,                           -- `user_id`，代表購物車中的使用者 ID，不能為 NULL
+    product_id INT NOT NULL,                        -- `product_id`，代表購物車中的產品 ID，不能為 NULL
+    many INT NOT NULL,                              -- `many`，代表該產品的購買數量，不能為 NULL
+    FOREIGN KEY (user_id) REFERENCES users(id),     -- `user_id` 外鍵，參考 `users` 資料表的 `id` 欄位
+    FOREIGN KEY (product_id) REFERENCES products(id)-- `product_id` 外鍵，參考 `products` 資料表的 `id` 欄位
+);
+
+CREATE TABLE orders (                             -- 建立 `orders` 資料表 下訂單的使用者
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    total_price FLOAT,
+    order_time DATETIME
+);
+
+CREATE TABLE order_details (            -- 建立 `order_details` 資料表，用來紀錄訂單的詳細資訊
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    many INT,
+    price FLOAT
+);
